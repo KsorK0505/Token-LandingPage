66263a25209106450c4950e03d3b92902c812167
Eugene <bob.parr.0101@gmail.com>
2025-01-07 07:06:39 -0500

    feat: code initial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from '@/assets/logo.svg';
+import '@/styles/App.css';
+
+function App() {
+  return (
+    <div className="text-center">
+      <h1 className="text-3xl font-bold underline text-blue-600">
+        Hello, Tailwind with React + TypeScript!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
